567ffdba16e6979e957b7b3df5fed5bcaa6d7950
zoar <zoar@k-side.net>
2018-06-08 11:11:24 +0900

    Create main script

---
+++ dailytweet.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# config: UTF-8
+
+require 'bundler'
+Bundler.require
+
+require 'json'
+require 'open-uri'
+require 'date'
+
+#メタ情報のセット
+current_path = File.expand_path(File.dirname(__FILE__))
+setting_file = current_path + '/setting.json'
+
+#六曜を取得する API エンドポイント
+api_uri = "http://api.sekido.info/qreki?output=jsonp&callback=null"
+
+funky = "ファンキ～～～～～～～～～～～～"
+
+message = ""
+
+tokenObj = open(setting_file) do |json_data|
+   JSON.load(json_data)
+end
+
+unless File.exist?(setting_file)
+  mkconfig = current_path + '/make-config.rb'
+  system mkconfig
+end
+
+def get_rokuyou(uri)
+   rokuyou_json = open(uri).read
+   return JSON.load(rokuyou_json[/\((.*?)\)/, 1])
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = tokenObj['ck']
+  config.consumer_secret = tokenObj['cs']
+  config.access_token = tokenObj['at']
+  config.access_token_secret = tokenObj['as']
+end
+
+if Date.today.wday == 5 then
+  message = funky
+else
+  message = get_rokuyou(api_uri)["rokuyou_text"]
+end
+
+client.update(message)
+
+exit 0
